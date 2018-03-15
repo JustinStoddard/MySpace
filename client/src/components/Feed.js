d84f5054526672f487bf73faed63a90de400fa2a
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-15 17:14:42 -0600

    New Updates

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -3,6 +3,28 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { Card, Image, Divider } from 'semantic-ui-react';
 
+// class Feed extends React.Component {
+//   state = { feeds: [] }
+
+//   render() {
+//     return (
+//       <div>
+//         <Card>
+//           { this.state.feeds.map( cat =>
+//             <Card key={feeds.id}>
+//               <h2>{feeds.name}</h2>
+//               <Image src={feeds.avatar} />
+//               <h3>{feeds.name}</h3>
+//               <h3>{feeds.description}</h3>
+//             </Card>
+//             )
+//           }
+//         </Card>
+//       </div>
+//     )
+//   }
+// }
+
 const Feed = () => (
   <div>
     <h1>Main Feed</h1>
